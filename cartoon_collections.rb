a5294d56f88cbb789f92f5b57e77e2874abf7326
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-01-28 14:40:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
 names = ["Doc", "Dopey", "Bashful", "Grumpy"]
 def roll_call_dwarves(names)
-  names.each_with_index |name, index|
+  names.each_with_index do |name, index|
   puts "#{index} #{name}"
 end
 end
